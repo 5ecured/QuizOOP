a330d3c9bf114f7fbe413d3ff5e3a96012b119bf
Edward <onirotto7585@pm.me>
2024-07-24 09:44:00 +0700

    added functionalities

--- question_model.py
+++ question_model.py
@@ -2,7 +2,3 @@ class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
-
-quest = Question('aaa','bbb')
-print(quest.text)
-print(quest.answer)
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,8 +1,12 @@
-class Brain:
-    def __init__(self, question_number, questions_list):
-        self.question_number = question_number
-        self.questions_list = questions_list
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
     
     def next_question(self):
-        pass
-    
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f'Q.{self.question_number}: {current_question.text} (True/False): ')
