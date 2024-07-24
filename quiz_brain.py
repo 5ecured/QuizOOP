7fd3e76e55356e72c38a48f498f2d316dc7bd580
Edward <onirotto7585@pm.me>
2024-07-24 09:20:33 +0700

    started quiz

---
+++ quiz_brain.py
@@ -0,0 +1,8 @@
+class Brain:
+    def __init__(self, question_number, questions_list):
+        self.question_number = question_number
+        self.questions_list = questions_list
+    
+    def next_question(self):
+        pass
+    
